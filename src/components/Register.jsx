276eb2419178bc8dc1a3bf28d80097b0ceea4240
Jeon Yong Hak <charismayoung1993@gmail.com>
2024-11-08 18:20:58 +0900

    이벤트 핸들러 작성하기

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,58 +7,72 @@ import { useState } from "react";
 // 4. 자기소개
 
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
 
   const onChangeName = (e) => {
-    setName(e.target.value);
+    setInput({
+      ...input,
+      name: e.target.value,
+    });
   };
 
   const onChangeBirth = (e) => {
-    setBirth(e.target.value);
+    setInput({
+      ...input,
+      birth: e.target.value,
+    });
   };
 
   const onChangeCountry = (e) => {
-    setCountry(e.target.value);
+    setInput({
+      ...input,
+      country: e.target.value,
+    });
   };
 
   const onChangeBio = (e) => {
-    setBio(e.target.value);
+    setInput({
+      ...input,
+      bio: e.target.value,
+    });
   };
 
   return (
     <>
       <div>
         <input
-          value={name}
+          value={input.name}
           onChange={onChangeName}
           placeholder={"이름"}
           type="text"
         />
       </div>
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
-        {birth}
+        <input value={} onChange={onChangeBirth} type="date" />
+        {input.birth}
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select value={input.country} onChange={onChangeCountry}>
           {/* 아무것도 설정되지 않은 목록을 추가하고 싶으면 빈 option 태그를 설정함 */}
           <option value="default"></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
           <option value="uk">영국</option>
         </select>
-        {country}
+        {input.country}
       </div>
       <div>
         <textarea
           placeholder="자기소개를 해주세요."
-          value={bio}
+          value={input.bio}
           onChange={onChangeBio}
         ></textarea>{" "}
-        {bio}
+        {input.bio}
       </div>
     </>
   );
