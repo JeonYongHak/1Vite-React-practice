f73d4fd6d3abdcf97c5d93912224a54579e9238c
Jeon Yong Hak <charismayoung1993@gmail.com>
2024-11-09 21:56:03 +0900

    카운터 앱 화면 ui 구성 with tailwindcss

---
+++ src/components/Controller.jsx
@@ -0,0 +1,31 @@
+const Controller = () => {
+  return (
+    <>
+      <div className="text-center font-semibold text-lg mb-4">
+        Simple Counter
+      </div>
+      <div className="flex space-x-4 justify-center">
+        <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-md shadow-lg transform transition duration-300 hover:scale-110 border-4 border-red-700">
+          -1
+        </button>
+        <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-md shadow-lg transform transition duration-300 hover:scale-110 border-4 border-orange-700">
+          -10
+        </button>
+        <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-md shadow-lg transform transition duration-300 hover:scale-110 border-4 border-yellow-700">
+          -100
+        </button>
+        <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-md shadow-lg transform transition duration-300 hover:scale-110 border-4 border-green-700">
+          +100
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md shadow-lg transform transition duration-300 hover:scale-110 border-4 border-blue-700">
+          +10
+        </button>
+        <button className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-md shadow-lg transform transition duration-300 hover:scale-110 border-4 border-purple-700">
+          +1
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Controller;
---
+++ src/components/Viewer.jsx
@@ -0,0 +1,8 @@
+const Viewer = () => {
+  return (
+    <>
+      <div className="text-center text-2xl font-bold mb-6">현재 카운트 : 0</div>
+    </>
+  );
+};
+export default Viewer;
