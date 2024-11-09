95fdcb338bc434053a19e5a8d96bff3946a44d48
Jeon Yong Hak <charismayoung1993@gmail.com>
2024-11-09 23:08:21 +0900

    카운터 앱 숫자 추가 및 빼기 기능 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
   return (
     <>
-      <h1 className="text-center py-5 text-4xl font-bold">Simple Counter</h1>
-      <section>
-        <Viewer />
-      </section>
-      <section>
-        <Controller />
-      </section>
+      <div className="m-20">
+        <h1 className="text-center py-5 text-4xl font-bold">Simple Counter</h1>
+        <section>
+          <Viewer count={count} />
+        </section>
+        <section>
+          <Controller onClickButton={onClickButton} />
+        </section>
+      </div>
     </>
   );
 }
--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,7 +1,9 @@
-const Viewer = () => {
+const Viewer = ({ count }) => {
   return (
     <>
-      <div className="text-center text-2xl font-bold mb-6">현재 카운트 : 0</div>
+      <div className="text-center text-2xl font-bold mb-6">
+        현재 카운트 : {count}
+      </div>
     </>
   );
 };
