a79bc651de387273b57feeeaf98a991df23b22ed
Jeon Yong Hak <charismayoung1993@gmail.com>
2024-11-10 21:28:18 +0900

    useEffect 의존성 배열 개념 정리 및 input으로 콘솔에 값 출력

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
 
   useEffect(() => {
-    console.log(`count: ${count}`);
-  }, [count]);
+    console.log(`input : ${input}`);
+  }, [count, input]);
   // 이때 새로고침을 누르게 되면, 콘솔의 입력 창이 초기화 됩니다.
- // 의존성 배열 : 
+  // 의존성 배열(deps) : deps에는 값을 여러 개 설정하여도 상관 없음
+  // 콘솔에 아무리 많은 값을 입력한다고 해도 콘솔에는 아무런 메시지도 나오지 않음
+  // 의존성 배열에 count값이 설정되어 있기에 count값이 변경이 되지 않아 useEffect가 실행되지 않는 것!
   const onClickButton = (value) => {
     setCount(count + value);
     if (value === 0) {
@@ -21,6 +24,15 @@ function App() {
     <>
       <div className="m-20">
         <h1 className="py-5 text-4xl font-bold text-center">Simple Counter</h1>
+        <section>
+          <input
+            className="px-4 py-2 font-bold text-center border border-slate-400 "
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+        </section>
         <section>
           <Viewer count={count} />
         </section>
